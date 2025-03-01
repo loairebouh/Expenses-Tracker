cfaa63cfa5c8f2fa34cb108c24c343debb862f2a
Loai Rebouh <loai_rebouh@icloud.com>
2025-03-02 00:07:34 +0100

    prepare the environement

---
+++ utils/styling.ts
@@ -0,0 +1,17 @@
+import { Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+const [shortDimension, longDimension] = width < height ? [width, height] : [height, width];
+
+const guidelineBaseWidth = 350;
+const guidelineBaseHeight = 680;
+
+export const scale = (size: number) => shortDimension / guidelineBaseWidth * size;
+export const verticalScale = (size: number) => longDimension / guidelineBaseHeight * size;
+export const moderateScale = (size: number, factor = 0.5) => size + (scale(size) - size) * factor;
+export const moderateVerticalScale = (size: number, factor = 0.5) => size + (verticalScale(size) - size) * factor;
+
+export const s = scale;
+export const vs = verticalScale;
+export const ms = moderateScale;
+export const mvs = moderateVerticalScale;
