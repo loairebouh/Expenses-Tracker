557913f04df52bdf4f7569eb722f7b71f28e1e11
Loai Rebouh <loai_rebouh@icloud.com>
2025-03-08 03:41:18 +0100

    init

---
+++ app/_layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import {Stack} from "expo-router"
+
+const _layout = () => {
+    return (
+        <Stack screenOptions={{headerShown: false}}>
+
+        </Stack>
+    );
+};
+
+export default _layout;
